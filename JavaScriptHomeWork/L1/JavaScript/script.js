231d6bd7c5164e0b320847c9cce48092cddcee0a
volodymyr rusnak <volodymyr_rusnak@epam.com>
2015-12-11 00:30:18 +0100

    js hw 1 fixes

--- JavaScriptHomeWork/L1/JavaScript/script.js
+++ JavaScriptHomeWork/L1/JavaScript/script.js
@@ -1,20 +1,17 @@
-// var chislo;
-// var stepen;
+var number = +prompt('Введите число');
+var exponent = +prompt('Введите степень, в которую вы хотите возвести число');
 
-var chislo = +prompt('Введите число');
-var stepen = +prompt('Введите степень, в которую вы хотите возвести число');
+function pow(a, b) {
+    var result = 1;
 
-function pow (a, b) {
-var result = 1;
+    for (var i = 0; i < exponent; i++) {
+        result = result * number;
+    }
 
-for (var i = 0; i < stepen; i++) {
-    result = result * chislo;
-}
-
-return result;
+    return result;
 
 }
 
-var powResult = pow(chislo, stepen);
+var powResult = pow(number, exponent);
 
-alert(chislo + ' в степени ' + stepen + ' = ' + powResult);
+alert(number + ' в степени ' + exponent + ' = ' + powResult);
